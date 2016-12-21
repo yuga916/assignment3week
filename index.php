182ed382606b8ef517408be252b7464b66ec71d5
yuga <yuga.ichinohe@gmail.com>
2016-12-21 23:46:47 +0800

    profile情報の追加

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ $time = date('H');
   <link rel="stylesheet" href="webroot/css/style.css">
   <!-- fontawesome -->
   <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+  <!-- w3.css -->
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3-theme-blue-grey.css">
 </head>
 <body>
   <div class="header">
@@ -26,13 +29,26 @@ $time = date('H');
   <div class="main">
     <div class="sidebar">
     <?php  if (6 <= $time && $time <= 11) : // 6時～11時の時間帯のとき ?>
-     <p>おはようございます
+     <p class="ana">おはようございます
      <?php  elseif (12 <= $time && $time <= 18) : // 12時〜20時の時間帯のとき ?>
-     <p>こんにちは
+     <p class="ana">こんにちは
      <?php  else : // それ以外の時間帯のとき ?>
-     <p>こんばんは
+     <p class="ana">こんばんは
      <?php endif; ?>
       、ゲストさん</p>
+      <br>
+      <div class="w3-card-2 w3-round w3-white" style="box-sizing: none;">
+              <div class="w3-container">
+               <h4 class="w3-center">My Profile</h4>
+               <p class="w3-center"><img src="images/profile.jpg" class="w3-circle" style="height:106px;width:106px" alt="Avatar"></p>
+               <hr>
+               <p><i class="fa fa-pencil fa-fw w3-margin-right w3-text-theme"></i>YUGA</p>
+               <p><i class="fa fa-home fa-fw w3-margin-right w3-text-theme"></i>TOKYO, JAPAN</p>
+              </div>
+        </div>
+       
+      <h2>過去の投稿</h2>
+
       <ul>
         <li><?php echo date("Y年m月"); ?>の投稿</li>
         <li><?php echo date( "Y年m月" , strtotime('-1 month' , time()) ) ;?>の投稿</li>
