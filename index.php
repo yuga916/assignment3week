b48c1d09dc7686da0ef863676cb170640c1efed9
yuga <yuga.ichinohe@gmail.com>
2017-01-02 16:29:09 +0800

    calendar追加、ヘッダー変更

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
-<?php
-$time = date('H');
-
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <title>Cafe Diary</title>
-  <link rel="stylesheet" href="webroot/css/style.css">
   <!-- fontawesome -->
   <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
   <!-- w3.css -->
   <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
   <link rel="stylesheet" href="http://www.w3schools.com/lib/w3-theme-blue-grey.css">
+
+  <!-- style-sheet -->
+  <link rel="stylesheet" href="webroot/css/style.css">
 </head>
 <body>
   <div class="header">
     <div class="header-left"><i class="fa fa-coffee" aria-hidden="true"></i>&nbsp;Cafe Diary</div>
     <div class="header-right">
-        
           <p>coffee calendar</p>
-        
     </div>
   </div>
 
@@ -37,7 +31,7 @@ $time = date('H');
      <?php endif; ?>
       、ゲストさん</p>
       <br>
-      <div class="w3-card-2 w3-round w3-white" style="box-sizing: none;">
+      <div class="w3-card-2 w3-round w3-white">
               <div class="w3-container">
                <h4 class="w3-center">My Profile</h4>
                <p class="w3-center"><img src="images/profile.jpg" class="w3-circle" style="height:106px;width:106px" alt="Avatar"></p>
@@ -58,6 +52,9 @@ $time = date('H');
     </div>
     
     <div class="contents">
+      <div class="contents-top">
+         <h1><?php echo date("Y年m月"); ?>の投稿</h1>
+      </div>
       <div class="contents-item">
         <h1>Spanish Chorizo, Egg and Fotina Cheese on Croissant Bun</h1>
         <img src="webroot/icon/coffee.png"><h2>starbucks</h2>
